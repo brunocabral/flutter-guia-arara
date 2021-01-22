7c6229932cc98a0719ab16ad6e1fce269aa5664f
Bruno Cabral <bruno_cabra1@yahoo.com.br>
2021-01-22 19:00:06 -0300

    Update menu routes

--- lib/widgets/grid_menu_widget.dart
+++ lib/widgets/grid_menu_widget.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:guia_arara/screens/about_screen.dart';
+import 'package:guia_arara/screens/climb_map_screen.dart';
+import 'package:guia_arara/screens/grade_screen.dart';
+import 'package:guia_arara/screens/info_screen.dart';
+import 'package:guia_arara/screens/road_map_screen.dart';
+import 'package:guia_arara/screens/sectors_screen.dart';
 import 'package:guia_arara/tiles/menu_tile.dart';
 
 class GridMenu extends StatelessWidget{
@@ -19,7 +25,7 @@ class GridMenu extends StatelessWidget{
             color: color,
             iconData: data.icon,
             title: data.title,
-            route: data.r,
+            route: data.route,
           );
         }).toList(),
       ),
@@ -29,22 +35,22 @@ class GridMenu extends StatelessWidget{
   List<Item> _buildItems(){
     List<Item> items = List<Item>();
     items.add(
-        Item(title: "Orientações Básicas", icon: Icons.announcement, route: )
+        Item(title: "Orientações Básicas", icon: Icons.announcement, route: InfoScreen())
     );
     items.add(
-        Item(title: "Como Chegar", icon: Icons.directions_car)
+        Item(title: "Como Chegar", icon: Icons.directions_car, route: RoadmapScreen())
     );
     items.add(
-        Item(title: "Mapa Local", icon: Icons.map)
+        Item(title: "Mapa Local", icon: Icons.map, route: ClimbmapScreen())
     );
     items.add(
-        Item(title: "Setores e Vias", icon: Icons.format_list_numbered)
+        Item(title: "Setores e Vias", icon: Icons.format_list_numbered, route: SectorsScreen())
     );
     items.add(
-        Item(title: "Graduação", icon: Icons.filter_9_plus)
+        Item(title: "Graduação", icon: Icons.filter_9_plus, route: GradeScreen())
     );
     items.add(
-        Item(title: "Sobre", icon: Icons.info_outline)
+        Item(title: "Sobre", icon: Icons.info_outline, route: AboutScreen())
     );
     return items;
   }
